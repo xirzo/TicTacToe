9f1e40866e947da5061734216ba8dc3e71bc3863
xirzo <easttem@gmail.com>
2025-06-17 15:02:41 +0300

    feat: vec2 expansion

--- bin/client.c
+++ bin/client.c
@@ -13,7 +13,24 @@ void button_on_pressed(button_t *btn) {
   printf("Button pressed: x: %f y: %f\n", btn->pos.x, btn->pos.y);
 }
 
+void init_buttons(button_t *btns, const vec2 *screen_center) {
+  for (int i = 0; i < BOARD_SIDE; i++) {
+    for (int j = 0; j < BOARD_SIDE; j++) {
+      button_t *btn = &btns[i + BOARD_SIDE * j];
+
+      vec2 pos;
+      vec2 offset = (vec2){ (i - 1) * 100, (j - 1) * 100 };
+      vec2_subtract(screen_center, &offset, &pos);
+      vec2 size = (vec2){ 80, 80 };
+      button_init(btn, pos, size, button_on_pressed);
+    }
+  }
+}
+
 int main(void) {
+  const vec2 SCREEN_CENTER = { (float)g_Properties.SCREEN_WIDTH / 2,
+                               (float)g_Properties.SCREEN_HEIGHT / 2 };
+
   game_state_t state;
 
   state_init(&state);
@@ -24,31 +41,19 @@ int main(void) {
 
   button_t *btns = malloc(BOARD_SIDE * BOARD_SIDE * sizeof(button_t));
 
-  for (int i = 0; i < BOARD_SIDE; i++) {
-    for (int j = 0; j < BOARD_SIDE; j++) {
-      button_t *btn = &btns[i + BOARD_SIDE * j];
-
-      vec2 pos = (vec2){ (float)g_Properties.SCREEN_WIDTH / 2 - (i - 1) * 100,
-                         (float)g_Properties.SCREEN_HEIGHT / 2 - (j - 1) * 100 };
-
-      vec2 size = (vec2){ 80, 80 };
-
-      button_init(btn, pos, size, button_on_pressed);
-    }
-  }
+  init_buttons(btns, &SCREEN_CENTER);
 
   while (!WindowShouldClose()) {
+    for (int i = 0; i < BOARD_SIDE * BOARD_SIDE; i++) {
+      button_check(&btns[i]);
+    }
+
     BeginDrawing();
 
     ClearBackground(BLACK);
 
-    Vector2 mouse_pos = GetMousePosition();
-
     for (int i = 0; i < BOARD_SIDE * BOARD_SIDE; i++) {
-      button_t *btn = &btns[i];
-
-      button_draw(btn);
-      button_check(btn);
+      button_draw(&btns[i]);
     }
 
     EndDrawing();
